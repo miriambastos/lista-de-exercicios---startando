fffa68ff7e13b6f3be19b706984ae4c729b370bd
Miriam Bastos <miriamrbastos1@gmail.com>
2024-07-01 22:06:35 -0300

    lista completa exceto 4, 7, 11 e 13

--- Program.cs
+++ Program.cs
@@ -41,18 +41,113 @@
 
 // Console.WriteLine($"Atualmente voce possui {idadeEmAnos} anos, {idadeEmMeses} meses, {idadeEmSemanas} semanas e {idadeEmDias} dias de idade" );
 
-// ----- Exercicio 8 ---------
-
-Console.WriteLine( "Informe a cotação do dolar na data de hoje" );
-double cotacaoDolar = double.Parse(Console.ReadLine());
-
-Console.WriteLine( "Informe o valor que deseja converter para reais" );
-double valorParaConversao = double.Parse(Console.ReadLine());
-
-double valorConvertido = Math.Round(valorParaConversao / cotacaoDolar, 2); //documentação informa que math.round aredonda o valor e temos a opção de inserir quantas casas decimais queremos após a virgula.
-
-Console.WriteLine($"O valor da cotação atual do dolar é de {cotacaoDolar}, desta forma com base no valor informado de {valorParaConversao} dolares, após a conversão para reais, o total é de {valorConvertido} reais" );
+// ------------- Exercicio 4 -------------
+//Não realizado 
 
 
+// -------------- Exercicio 5 ---------------
+
+// Console.WriteLine("Digite o valor de A: ");
+// string valorA = Console.ReadLine();
+        
+// Console.WriteLine("Digite o valor de B: ");
+// string valorB = Console.ReadLine();
+
+// Console.WriteLine($"Valor de A = {valorA}, B = {valorB}");
+ 
+// string valorAposTroca = valorA;  // Trocando os valores
+// valorA = valorB;
+// valorB = valorAposTroca;
+
+//     // Valor após a troca
+// Console.WriteLine($"Os valores após a troca é: A = {valorA}, B = {valorB}");
+
+//-------------- Exercicio 6 -----------
+
+// Console.WriteLine("Digite o comprimento da caixa: ");
+// double comprimento = double.Parse(Console.ReadLine());
+        
+// Console.WriteLine("Digite a largura da caixa: ");
+// double largura = double.Parse(Console.ReadLine());
+        
+// Console.WriteLine("Digite a altura da caixa: ");
+// double altura = double.Parse(Console.ReadLine());
+
+// double volume = comprimento * largura * altura;
+    
+// Console.WriteLine($"O volume da caixa retangular é: {volume}");
 
 
+// -------------- Exercicio 7 ------------
+//Não realizado 
+
+
+// -------------- Exercicio 8 ------------
+
+// Console.WriteLine( "Informe a cotação do dolar na data de hoje" );
+// double cotacaoDolar = double.Parse(Console.ReadLine());
+
+// Console.WriteLine( "Informe o valor que deseja converter para reais" );
+// double valorParaConversao = double.Parse(Console.ReadLine());
+
+// double valorConvertido = Math.Round(valorParaConversao / cotacaoDolar, 2); //documentação informa que math.round aredonda o valor e temos a opção de inserir quantas casas decimais queremos após a virgula.
+
+// Console.WriteLine($"O valor da cotação atual do dolar é de {cotacaoDolar}, desta forma com base no valor informado de {valorParaConversao} dolares, após a conversão para reais, o total é de {valorConvertido} reais" );
+
+// ---------- Exercicio 9 ----------
+
+//  Console.WriteLine("Digite o valor de A: ");
+//  int valorA = int.Parse(Console.ReadLine());
+        
+//  Console.WriteLine("Digite o valor de B: ");
+//  int valorB = int.Parse(Console.ReadLine());
+        
+//  Console.WriteLine("Digite o valor de C: ");
+//  int valorC = int.Parse(Console.ReadLine());
+   
+//  int soma = valorA + valorB + valorC;
+
+// // Calcula o quadrado 
+// int somaQuadrado  = soma * soma;
+
+// Console.WriteLine($"O quadrado da soma dos valores é: {somaQuadrado}");
+
+
+// ---------- Exercicio 10 ---------
+
+// Console.WriteLine("Digite o valor de A: ");
+// double valorA = double.Parse(Console.ReadLine());
+        
+// Console.WriteLine("Digite o valor de B: ");
+// double valorB = double.Parse(Console.ReadLine());
+
+// // operações 
+// double soma = valorA + valorB;
+// double subtracao = valorA - valorB;
+// double multiplicacao = valorA * valorB;
+// double divisao = valorA / valorB;
+
+// // resultados
+// Console.WriteLine($"A soma de A e B é: {soma}");
+// Console.WriteLine($"A subtração de A e B é: {subtracao}");
+// Console.WriteLine($"A multiplicação de A e B é: {multiplicacao}");
+// Console.WriteLine($"A divisão de A e B é: {divisao}");
+
+
+// -------- Exercicio 11 ----------
+//Não realizado 
+
+// --------- Exercicio 12 ---------
+
+// Console.Write("Digite um valor numérico inteiro: ");
+// int valor = int.Parse(Console.ReadLine());
+
+// int valorSucessor = valor + 1;
+// int valorAntecessor = valor - 1;
+
+// Console.WriteLine($"O valor digitado é: {valor}");
+// Console.WriteLine($"O sucessor de {valor} é: {valorSucessor}");
+// Console.WriteLine($"O antecessor de {valor} é: {valorAntecessor}");
+
+// -------- Exercicio 13 ----------
+//Não realizado 
